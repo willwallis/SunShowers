4bebb05aa39df66e467ee898618556848b257e71
Will <will@knewto.com>
2015-09-08 18:35:45 -0700

    Completed Lesson 4B

---
+++ app/src/androidTest/java/com/knewto/www/sunshowers/data/TestWeatherContract.java
@@ -0,0 +1,33 @@
+package com.knewto.www.sunshowers.data;
+
+/**
+ * Created by willwallis on 8/23/15.
+ */
+import android.net.Uri;
+import android.test.AndroidTestCase;
+
+/*
+    Students: This is NOT a complete test for the WeatherContract --- just for the functions
+    that we expect you to write.
+ */
+public class TestWeatherContract extends AndroidTestCase {
+
+    // intentionally includes a slash to make sure Uri is getting quoted correctly
+    private static final String TEST_WEATHER_LOCATION = "/North Pole";
+    private static final long TEST_WEATHER_DATE = 1419033600L;  // December 20th, 2014
+
+    /*
+        Students: Uncomment this out to test your weather location function.
+     */
+    public void testBuildWeatherLocation() {
+        Uri locationUri = WeatherContract.WeatherEntry.buildWeatherLocation(TEST_WEATHER_LOCATION);
+        assertNotNull("Error: Null Uri returned.  You must fill-in buildWeatherLocation in " +
+                        "WeatherContract.",
+                locationUri);
+        assertEquals("Error: Weather location not properly appended to the end of the Uri",
+                TEST_WEATHER_LOCATION, locationUri.getLastPathSegment());
+        assertEquals("Error: Weather location Uri doesn't match our expected result",
+                locationUri.toString(),
+                "content://com.knewto.www.sunshowers/weather/%2FNorth%20Pole");
+    }
+}
